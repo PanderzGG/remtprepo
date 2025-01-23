4c31a30d25801c8115c8779bc131a3113d2643d8
PanderzGG <125514785+PanderzGG@users.noreply.github.com>
2025-01-23 07:32:03 +0100

    initial commit

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<Person> person = new List<Person>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveName_Click(object sender, RoutedEventArgs e)
+        {
+            Person p = new Person(tbname.Text, Convert.ToInt32(tbAlter.Text));
+            person.Add(p);
+
+            // Listen triggered bei der Aktualisierung die Benutzerfläche nicht.
+            GridName.Items.Refresh();
+
+
+            GridName.ItemsSource = person;
+            tbname.Text = "";
+            tbAlter.Text = "";
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+    }
+}
